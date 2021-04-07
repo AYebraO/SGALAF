4f39095bf721fdcc283b80ccebbcb1eedbd68603
jlopezc <jlopezc@aic.pgjguanajuato.gob.mx>
2021-04-07 16:55:10 -0500

    cambio marcos 1

--- Inicio.xaml.vb
+++ Inicio.xaml.vb
@@ -3,7 +3,7 @@
 
     End Sub
     Private Sub TextBox_TextChanged(sender As Object, e As TextChangedEventArgs)
-
+        'cambio para subir a la rama
     End Sub
 
     Private Sub DataGrid_SelectionChanged(sender As Object, e As SelectionChangedEventArgs)
