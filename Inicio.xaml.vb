a398899714daca883eb6da39db9a96591ad28093
jlopezc <jlopezc@aic.pgjguanajuato.gob.mx>
2021-04-13 14:05:39 -0500

    dbconexion

--- Inicio.xaml.vb
+++ Inicio.xaml.vb
@@ -2,16 +2,56 @@
 Imports System.Data
 Imports System.Data.OleDb
 Imports System.Media.SoundPlayer
+Imports System.Collections.ObjectModel
+Imports System.Diagnostics
 
 
 Public Class Inicio
 
 
+    'Create ADO.NET objects.
+    Private myConn As SqlConnection
+    Private myCmd As SqlCommand
+    Private myReader As SqlDataReader
+    Private results As String
+
+    'Defines the customer object
+    Public Class Persona
+        Public Property Nombre() As String
+        Public Property Edad() As Int32
+        Public Property Genero() As String
+        Public Property Acento() As String
+        Public Property ritmo() As String
+    End Class
+
 
     Private Sub Inicio_Loaded(ByVal sender As System.Object, ByVal e As System.Windows.RoutedEventArgs) Handles MyBase.Loaded
 
+        myConn = New SqlConnection("Initial Catalog=dbLAF;" &
+                                   "Data Source=localhost;Integrated Security=SSPI;")
+        myCmd = myConn.CreateCommand
+        myCmd.CommandText = "SELECT nombre, edad FROM Persona"
 
-        dtPersona.SetBinding(Persona, "Persona")
+        'Open the connection.
+        myConn.Open()
+
+        myReader = myCmd.ExecuteReader()
+
+
+
+        'Concatenate the query result into a string.
+        Do While myReader.Read()
+            results = results & myReader.GetString(0) & vbTab &
+            myReader.GetInt32(1) & vbLf
+        Loop
+        'Display results.
+        MessageBox.Show(results)
+
+        'Close the reader and the database connection.
+        myReader.Close()
+        myConn.Close()
+
+        ' dtPersona.SetBinding(Persona, "Persona")
     End Sub
     Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
 
@@ -25,6 +65,13 @@ Public Class Inicio
     End Sub
 
     Private Sub btnGuardarPersona_Click(sender As Object, e As RoutedEventArgs) Handles btnGuardarPersona.Click
+        InitializeComponent()
+
+        'GetData() creates a collection of Customer data from a database
+        Dim custdata As ObservableCollection(Of Persona) = GetData()
+
+        'Bind the DataGrid to the customer data
+        dtPersona.DataContext = custdata
 
 
     End Sub
